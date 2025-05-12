b4f6bd69b51ae4fc34f8bde18d98191970773d2f
Nina Schmid <nina.schmid@uni-bonn.de>
2025-05-12 11:33:58 +0200

    update manhole names

--- analysis/scripts/plotting_tools.py
+++ analysis/scripts/plotting_tools.py
@@ -16,17 +16,45 @@ set_fontsize()
 
 # Mapping of manhole codes to real-world names
 manhole_names = {
-    "MUC012": "Am Langwieder Bach",
-    "MUC060": "Gräfelfinger Str.", # (not including Waldwiesenstr.)
-    "MUC362": "Schenkendorfstr.",
-    "MUC348": "Schmidbartlanger",
-    "MUC494": "Savitstr.",
-    "MUC608": "Gyßlinger Becken",
-    "MUC600": "Leopoldstr.",
-    "MUC112": "Botanischer Garten", # (not including Menzinger Str. )
-    "MUC560": "Wintersteinstr.", # Hasenbergl
-    "MUC434": "WWT Gut Großlappen",
-    "MUC562": "WWT Gut Marienhof",
-    "MUC586": "Gesamt",}
+    "MUC560": "N_Ua", # Wintersteinstr.", # Hasenbergl
+    "MUC348": "N_Ub", # "Schmidbartlanger",
+    "MUC012": "N_Uc", # "Am Langwieder Bach",
+    "MUC112": "C_U", # Botanischer Garten/Menzinger Str.
+    
+    "MUC060": "S_Ua", # Gräfelfinger Str. / Waldwiesenstr.
+    "MUC612": "S_Ub", # Neue Messstelle 1
+    "MUC616": "S_M1", # Neue Messstelle 4
+    "MUC600": "S_M2", # "Leopoldstr.",
+    
+    "MUC608": "S_M3", # "Gyßlinger Becken",
+    "MUC362": "S_M4", # "Schenkendorfstr.",
+    "MUC614": "E_U", # Neue Messstelle 2
+    "MUC494": "E_M", # "Savitstr.",
 
-start_date = "2020-03-01"
+    "MUC562": "N_D", # "WWT Gut Marienhof",
+    "MUC434": "SCE_D1", # "Zulauf Gut Großlappen",
+    "MUC596": "SCE_D2", # "WWT Gut Großlappen",    
+    "MUC586": "Overall",}
+
+manhole_clear_names = {
+    "Wintersteinstr.": "N_Ua", # Wintersteinstr.", # Hasenbergl
+    "Schmidbartlangerstr.": "N_Ub", # "Schmidbartlanger",
+    "Am\nLangwieder\nBach": "N_Uc", # "Am Langwieder Bach",
+    "Botanischer\nGarten": "C_U", # Botanischer Garten/Menzinger Str.
+    
+    "Gräfelfinger\nStr.": "S_Ua", # Gräfelfinger Str. / Waldwiesenstr.
+    "Neue\nMessstelle 1": "S_Ub", # Neue Messstelle 1
+    "Neue\nMessstelle 4": "S_M1", # Neue Messstelle 4
+    "Leopoldstr.": "S_M2", # "Leopoldstr.",
+    \
+    "Gyßlinger Becken": "S_M3", # "Gyßlinger Becken",
+    "Schenkendorfstr.": "S_M4", # "Schenkendorfstr.",
+    "Neue\nMessstelle 2": "E_U", # Neue Messstelle 2
+    "Savitstr.": "E_M", # "Savitstr.",
+
+    "WWT Gut Marienhof": "N_D", # "WWT Gut Marienhof",
+    "Zulauf\nGut Großlappen": "SCE_D1", # "Zulauf Gut Großlappen",
+    "WWT Gut Großlappen": "SCE_D2", # "WWT Gut Großlappen",    
+    "Gesamt": "Overall",}
+
+start_date = "2020-03-02"
