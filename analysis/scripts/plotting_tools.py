a3a37369059575cb4761d5fa44ed955eb53839dd
Nina Schmid <nina.schmid@uni-bonn.de>
2025-07-23 09:00:01 +0200

    update plotting scripts

--- analysis/scripts/plotting_tools.py
+++ analysis/scripts/plotting_tools.py
@@ -5,14 +5,36 @@ def set_fontsize(base_fontsize=15):
     fontsize = base_fontsize
     plt.rcParams.update({
         'font.size': fontsize,
-        'axes.titlesize': fontsize * 1.2,
+        'axes.titlesize': fontsize * 1,
         'axes.labelsize': fontsize,
         'xtick.labelsize': fontsize * 0.8,
         'ytick.labelsize': fontsize * 0.8,
-        'legend.fontsize': fontsize * 0.9
+        'legend.fontsize': fontsize * 0.8,
+        'font.family': "Arial"
     })
 
+plt.style.use('default')
+
+cm = 2/2.54            # handy conversion factor → inches per centimetre (doubled, to fit google slides settings)
+
 set_fontsize()
+
+green = "#83AF63"
+blue = "#3875AF"
+orange="#F28C28"
+red = "#880000"
+teal = "#4C8670"
+purple = "#874C82"
+brown = "#8B4513"
+tan = "#C68B70"
+dark_green = "#2F4F4F"
+black = "#000000"
+dark_grey = "#595959"
+medium_grey = "#a2a2a2"
+light_grey = "#cccccc"
+very_light_grey = "#eeeeee"
+white = "#ffffff"
+
 
 # Mapping of manhole codes to real-world names
 manhole_names = {
@@ -56,5 +78,25 @@ manhole_clear_names = {
     "Zulauf\nGut Großlappen": "SCE_D1", # "Zulauf Gut Großlappen",
     "WWT Gut Großlappen": "SCE_D2", # "WWT Gut Großlappen",    
     "Gesamt": "Overall",}
+
+manhole_upstream_ordering = {
+    "N_Ua": ["N_Ua"],
+    "N_Ub": ["N_Ub"],
+    "N_Uc": ["N_Uc"],
+    "C_U": ["C_U"],
+
+    "S_Ua": ["S_Ua"],
+    "S_Ub": ["S_Ub"],
+    "E_U": ["E_U"]}
+manhole_upstream_ordering["S_M1"] = ["S_M1"] + manhole_upstream_ordering["S_Ua"] + manhole_upstream_ordering["S_Ub"]
+manhole_upstream_ordering["S_M2"] = ["S_M2"] + manhole_upstream_ordering["S_M1"] 
+manhole_upstream_ordering["S_M3"] = ["S_M3"] + manhole_upstream_ordering["S_M2"] 
+manhole_upstream_ordering["S_M4"] = ["S_M4"] + manhole_upstream_ordering["S_M3"] 
+manhole_upstream_ordering["E_M"] = ["E_M"] + manhole_upstream_ordering["E_U"]
+manhole_upstream_ordering["SCE_D1"] = ["SCE_D1"] + manhole_upstream_ordering["S_M4"] + manhole_upstream_ordering["E_M"] + manhole_upstream_ordering["C_U"] 
+manhole_upstream_ordering["SCE_D2"] = manhole_upstream_ordering["SCE_D1"]
+manhole_upstream_ordering["N_D"] = ["N_D"] + ["N_Ua", "N_Ub", "N_Uc"]
+manhole_upstream_ordering["Overall"] = manhole_upstream_ordering["SCE_D2"] + manhole_upstream_ordering["SCE_D1"] + manhole_upstream_ordering["N_D"]
+
 
 start_date = "2020-03-02"
